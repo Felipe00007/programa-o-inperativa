af44600a89e4fca01c973a9ffdbdd3ea19c09e29
FELIPE RODRIGUES DE MELO <85718153+Felipe00007@users.noreply.github.com>
2021-06-16 13:31:19 -0300

    Update fatorial.js

--- aula 7/fatorial.js
+++ aula 7/fatorial.js
@@ -1,6 +1,9 @@
-var fatorial = 5;
-var resultado = fatorial;
-for (var i = 1; i < fatorial; i++) {
-    resultado *= i;
-}
-console.log(resultado)
+function fatorial(n){
+    var resultado = n;
+    for(var i = 1; i<n; i++){
+      resultado = resultado * i;
+    }
+    return resultado;
+  }
+  
+  console.log(fatorial(5));
